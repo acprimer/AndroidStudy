c2e31d26b169d975f3fd57116e1a26a446f001fa
yaodh <yaodh@rd.netease.com>
2016-07-05 20:43:24 +0800

    Android Study + DrawerLayout

--- app/src/main/java/com/study/yaodh/androidstudy/activity/DrawerActivity.java
+++ app/src/main/java/com/study/yaodh/androidstudy/activity/DrawerActivity.java
@@ -1,12 +1,16 @@
 package com.study.yaodh.androidstudy.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import com.study.yaodh.androidstudy.R;
 
@@ -16,23 +20,78 @@ import com.study.yaodh.androidstudy.R;
 public class DrawerActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
     private String[] mPlanetTitles;
     private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
+    private TextView mDrawerList;
+    private TextView textView;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
 
+        initToolbar();
+
         mPlanetTitles = getResources().getStringArray(R.array.menu);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList = (TextView) findViewById(R.id.left_drawer);
+        textView = (TextView) findViewById(R.id.text);
 
-        mDrawerList.setAdapter(new ArrayAdapter<String>(
+//        mDrawerList.setAdapter(new ArrayAdapter<>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                mPlanetTitles
+//        ));
+//        mDrawerList.setOnItemClickListener(this);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
                 this,
-                android.R.layout.simple_list_item_1,
-                mPlanetTitles
-        ));
-        mDrawerList.setOnItemClickListener(this);
+                mDrawerLayout,
+                R.drawable.ic_logo,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDrawerLayout.openDrawer(mDrawerList);
+            }
+        });
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -41,5 +100,8 @@ public class DrawerActivity extends AppCompatActivity implements AdapterView.OnI
     }
 
     private void selectItem(int position) {
+        textView.setText("You clicked " + position);
+
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
 }
