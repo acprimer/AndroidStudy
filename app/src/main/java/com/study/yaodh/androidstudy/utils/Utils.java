a7c5734d0245945d72fd948904082f5961db5b76
acprimer <729101421@qq.com>
2016-08-15 20:34:12 +0800

    round corner bitmap

--- app/src/main/java/com/study/yaodh/androidstudy/utils/Utils.java
+++ app/src/main/java/com/study/yaodh/androidstudy/utils/Utils.java
@@ -1,6 +1,14 @@
 package com.study.yaodh.androidstudy.utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 
 /**
  * Created by yaodh on 16/7/10.
@@ -9,5 +17,31 @@ public class Utils {
     public static int dip2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 获取圆角的bitmap
+     * @param bitmap 原始bitmap
+     * @param corner 圆角大小
+     * @return
+     */
+    public static Bitmap getRoundCornerBitmap(Bitmap bitmap, float corner) {
+        if (bitmap == null) return bitmap;
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        Bitmap roundCornerBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(roundCornerBitmap);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setAntiAlias(true);
+        Rect rect = new Rect(0, 0, width, height);
+        RectF rectF = new RectF(rect);
+        canvas.drawRoundRect(rectF, corner, corner, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return roundCornerBitmap;
     }
 }
