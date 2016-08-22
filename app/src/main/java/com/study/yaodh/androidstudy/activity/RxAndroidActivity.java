5a71cd105c6b6789148ec92309449383fd87ec45
acprimer <729101421@qq.com>
2016-08-22 09:44:05 +0800

    + rxandroid

---
+++ app/src/main/java/com/study/yaodh/androidstudy/activity/RxAndroidActivity.java
@@ -0,0 +1,14 @@
+package com.study.yaodh.androidstudy.activity;
+
+import android.view.View;
+
+public class RxAndroidActivity extends BaseActivity {
+    @Override
+    protected void initContent() {
+        super.initContent();
+
+    }
+
+    public void onCalc(View view) {
+    }
+}
