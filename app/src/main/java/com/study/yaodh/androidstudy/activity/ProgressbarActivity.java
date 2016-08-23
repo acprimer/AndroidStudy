c02877473ddfe3fa3517db0460fd28359908ba5c
acprimer <729101421@qq.com>
2016-08-23 19:59:14 +0800

    + custom progressbar

--- app/src/main/java/com/study/yaodh/androidstudy/activity/ProgressbarActivity.java
+++ app/src/main/java/com/study/yaodh/androidstudy/activity/ProgressbarActivity.java
@@ -1,15 +1,31 @@
 package com.study.yaodh.androidstudy.activity;
 
+import android.animation.ValueAnimator;
+import android.databinding.DataBindingUtil;
+
 import com.study.yaodh.androidstudy.R;
+import com.study.yaodh.androidstudy.databinding.ActivityProgressbarBinding;
 
 public class ProgressbarActivity extends BaseActivity {
-
-    @Override
-    protected int getLayoutId() {
-        return R.layout.activity_progressbar;
-    }
+    ActivityProgressbarBinding binding;
 
     @Override
     protected void initContent() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_progressbar);
+        simulateProgress();
+    }
+
+    private void simulateProgress() {
+        ValueAnimator animator = ValueAnimator.ofInt(0, 100);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int progress = (int) animation.getAnimatedValue();
+                binding.customProgress.setProgress(progress);
+            }
+        });
+        animator.setRepeatCount(0);
+        animator.setDuration(4000);
+        animator.start();
     }
 }
